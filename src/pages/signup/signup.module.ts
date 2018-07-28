30cb8102ab2ee10f3d05dfebc7ef459607622db6
LucasRCunha <lucasrcunha01@gmail.com>
2018-07-28 15:15:17 -0300

    Povoando dinamicamente estado e cidade

--- src/pages/signup/signup.module.ts
+++ src/pages/signup/signup.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { SignupPage } from './signup';
+import { EstadoService } from '../../services/domain/estado.service';
+import { CidadeService } from '../../services/domain/cidade.service';
  @NgModule({
   declarations: [
     SignupPage,
@@ -8,5 +10,9 @@ import { SignupPage } from './signup';
   imports: [
     IonicPageModule.forChild(SignupPage),
   ],
+  providers: [
+    CidadeService,
+    EstadoService
+  ]
 })
 export class SignupPageModule {}
