24556d0377d690df8ac70b04ca39d01c06c775bc
LucasRCunha <lucasrcunha01@gmail.com>
2018-08-16 17:53:24 -0300

    Carregando produtos de uma dada categoria

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -2,27 +2,27 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { ProdutoDTO } from '../../models/produto.dto';
 import { API_CONFIG } from '../../config/api.config';
- @IonicPage()
+import { ProdutoService } from '../../services/domain/produto.service';
+@IonicPage()
 @Component({
   selector: 'page-produtos',
   templateUrl: 'produtos.html',
 })
 export class ProdutosPage {
-   items : ProdutoDTO[];
-   constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
-   ionViewDidLoad() {
-    this.items = [
-      {
-        id: "1",
-        nome: 'Mouse',
-        preco: 80.99
+  items : ProdutoDTO[];
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    public produtoService: ProdutoService) {
+    }
+    
+    ionViewDidLoad() {
+      let categoria_id = this.navParams.get('categoria_id');
+      this.produtoService.findByCategoria(categoria_id)
+      .subscribe(response => {
+        this.items = response['content'];
       },
-      {
-        id: "2",
-        nome: 'Teclado',
-        preco: 100.00
-      }
-    ]
-  };
-}
+      error => {});
+    }
+    
+  }
