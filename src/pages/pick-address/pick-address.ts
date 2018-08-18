ebc376e60ec3f2cae1c983941b8e5d8402c59531
LucasRCunha <lucasrcunha01@gmail.com>
2018-08-18 16:15:10 -0300

    Tela de escolha de pagamento

--- src/pages/pick-address/pick-address.ts
+++ src/pages/pick-address/pick-address.ts
@@ -48,10 +48,10 @@ export class PickAddressPage {
         this.navCtrl.setRoot('HomePage');
       }
     }
-    
+
     nextPage(item: EnderecoDTO) {
       this.pedido.enderecoDeEntrega = {id: item.id};
-      console.log(this.pedido); 
+       this.navCtrl.push('PaymentPage', {pedido: this.pedido});
     }
   
     
