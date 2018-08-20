9d278a05a19c526615c57b47d42ec242c1c0710a
LucasRCunha <lucasrcunha01@gmail.com>
2018-08-20 19:02:46 -0300

    Refresher

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -11,18 +11,24 @@ import { LoadingController } from 'ionic-angular/components/loading/loading-cont
   templateUrl: 'produtos.html',
 })
 export class ProdutosPage {
+
   items : ProdutoDTO[];
+
   constructor(
     public navCtrl: NavController, 
     public navParams: NavParams,
     public produtoService: ProdutoService,
     public loadingCtrl: LoadingController) {
-    }
-    
-    ionViewDidLoad() {
-      let categoria_id = this.navParams.get('categoria_id');
-      let loader = this.presentLoading();
-      this.produtoService.findByCategoria(categoria_id)
+  }
+
+  ionViewDidLoad() {
+    this.loadData();
+  }
+
+  loadData() {
+    let categoria_id = this.navParams.get('categoria_id');
+    let loader = this.presentLoading();
+    this.produtoService.findByCategoria(categoria_id)
       .subscribe(response => {
         this.items = response['content'];
         loader.dismiss();
@@ -31,29 +37,35 @@ export class ProdutosPage {
       error => {
         loader.dismiss();
       });
-    }
-
-    loadImageUrls() {
-      for (var i=0; i<this.items.length; i++) {
-        let item = this.items[i];
-        this.produtoService.getSmallImageFromBucket(item.id)
-          .subscribe(response => {
-            item.imageUrl = `${API_CONFIG.bucketBaseUrl}/prod${item.id}-small.jpg`;
-          },
-          error => {});
-      }
-    }
-    
-    showDetail(produto_id : string) {
-      this.navCtrl.push('ProdutoDetailPage', {produto_id: produto_id});
-    }
-
-    presentLoading() {
-      let loader = this.loadingCtrl.create({
-        content: "Aguarde..."
-      });
-      loader.present();
-      return loader;
-    }
-    
   }
+
+  loadImageUrls() {
+    for (var i=0; i<this.items.length; i++) {
+      let item = this.items[i];
+      this.produtoService.getSmallImageFromBucket(item.id)
+        .subscribe(response => {
+          item.imageUrl = `${API_CONFIG.bucketBaseUrl}/prod${item.id}-small.jpg`;
+        },
+        error => {});
+    }
+  }  
+
+  showDetail(produto_id : string) {
+    this.navCtrl.push('ProdutoDetailPage', {produto_id: produto_id});
+  }
+
+  presentLoading() {
+    let loader = this.loadingCtrl.create({
+      content: "Aguarde..."
+    });
+    loader.present();
+    return loader;
+  }
+
+  doRefresh(refresher) {
+    this.loadData();
+    setTimeout(() => {
+      refresher.complete();
+    }, 1000);
+  }
+}
